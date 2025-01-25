74079e4f37fb488f83f0490264d9df5a110882ee
Iride Massidda <lorenzomassidda@hotmail.com>
2025-01-25 16:40:26 +0100

    fix dying down

--- scenes/gameplay/death.gd
+++ scenes/gameplay/death.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+@export var need_bubble:=false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +9,6 @@ func _ready() -> void:
 
 func on_body_entered(node:Node2D) -> void:
 	var player := node as Player
-	if player and player.is_in_bubble():
-		player.die()
+	if player:
+		if not need_bubble or player.is_in_bubble():
+			player.die()
