4d97ab0852005c1a7d2fd2062a6a278da7dc9173
Iride Massidda <lorenzomassidda@hotmail.com>
2025-01-25 11:29:47 +0100

    feat: bubbling up

--- scenes/bubble.gd
+++ scenes/bubble.gd
@@ -2,6 +2,12 @@ class_name Bubble extends Area2D
 
 @export var speed := 10
 var direction:=Vector2.RIGHT
-
+var player_index:=0
 func _physics_process(delta: float) -> void:
 	translate(direction*speed*delta)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var player := body as Player
+	if player and player_index != player.player_index:
+		player.bubbled()
--- scenes/gameplay/player/player.gd
+++ scenes/gameplay/player/player.gd
@@ -1,5 +1,5 @@
 class_name Player extends CharacterBody2D
-
+@export var player_index:=0
 @export_group("Actions")
 @export var move_left_action:= "move_left"
 @export var move_right_action:= "move_right"
@@ -35,8 +35,7 @@ class_name Player extends CharacterBody2D
 @export var jump_buffer := 0.1
 
 @onready var _aerial_jumps := number_of_aerial_jumps
-
-
+@onready var state_chart: StateChart = $StateChart
 
 var facing_right := true
 var dash_direction := Vector2.ZERO
@@ -62,28 +61,36 @@ func calculate_fall_multiplier() -> float:
 func die() -> void:
 	Game.restart_scene()
 
+func bubbled() -> void:
+	state_chart.send_event("bubbled")
+
+func get_closest_axis(direction:Vector2) -> Vector2:
+	return direction.snapped(Vector2.ONE).normalized()
+
+func update_facing() -> void:
+	facing_right = velocity.x > 0
+
 @onready var gun_pivot: Node2D = $Pivot
 @onready var gun: Node2D = $Pivot/Gun
 @export var bubble: PackedScene
 
-func _unhandled_input(event: InputEvent) -> void:
+
+
+func _on_normal_state_state_input(event: InputEvent) -> void:
 	if event.is_action_pressed(shoot_action):
 		var bub :Bubble= bubble.instantiate()
 		bub.direction = Vector2.RIGHT.rotated(gun_pivot.rotation)
+		bub.player_index = player_index
 		bub.global_position=gun.global_position
 		get_tree().current_scene.add_child(bub)
 
-func _process(delta: float) -> void:
+
+func _on_normal_state_state_processing(delta: float) -> void:
 	var input := Input.get_vector(gun_left_action,gun_right_action,gun_up_action,gun_down_action).normalized()
 	if input != Vector2.ZERO:
 		gun_pivot.rotation = atan2(input.y,input.x)
 
-func _physics_process(delta: float) -> void:
-	# Movement
-	var horizontal_input := Input.get_axis(move_left_action,move_right_action)
-
-	var acceleration := ground_acceleration if is_on_floor() else air_acceleration
-
+func accelerate(horizontal_input:float,acceleration:float,delta:float,max_speed:float) -> void:
 	if horizontal_input < 0.0:
 		# Move left
 		update_facing()
@@ -96,10 +103,18 @@ func _physics_process(delta: float) -> void:
 		if velocity.x < 0.0:
 			velocity.x = 0.0
 		velocity.x = lerp(velocity.x, movement_speed, acceleration * delta)
+
 	else:
 		# Stop
 		velocity.x = lerp(velocity.x, 0.0, acceleration * deceleration_multiplier * delta)
 	velocity.x = clamp(velocity.x, -movement_speed,movement_speed)
+
+func _on_normal_state_state_physics_processing(delta: float) -> void:
+	var horizontal_input := Input.get_axis(move_left_action,move_right_action)
+
+	var acceleration := ground_acceleration if is_on_floor() else air_acceleration
+
+	accelerate(horizontal_input,acceleration,delta,movement_speed)
 
 	# Jump
 	if (Input.is_action_just_pressed(jump_action) or time_since_jump_press < jump_buffer) and (is_on_floor() or time_since_grounded < coyote_time):
@@ -131,9 +146,21 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-func get_closest_axis(direction:Vector2) -> Vector2:
-	return direction.snapped(Vector2.ONE).normalized()
+@onready var bubble_shape: CollisionShape2D = $BubbleShape
+@export_group("Bubble State")
+@export var bubbled_up_speed := 1000.0
+@export var bubbled_horizontal_speed := 1000.0
 
+func _on_bubbled_state_state_entered() -> void:
+	bubble_shape.show()
 
-func update_facing() -> void:
-	facing_right = velocity.x > 0
+func _on_bubbled_state_state_exited() -> void:
+	bubble_shape.hide()
+
+func _on_bubbled_state_state_physics_processing(delta: float) -> void:
+	var horizontal_input := Input.get_axis(move_left_action,move_right_action)
+	var acceleration := air_acceleration
+	accelerate(horizontal_input,acceleration,delta,bubbled_horizontal_speed)
+
+	velocity.y = -bubbled_up_speed
+	move_and_slide()
